a12e5f84a226a8bfac78887a8502bab465877eba
Nikitin_AA <nikitin_aa@businessculture.ru>
2021-04-18 19:55:44 +0400

    completed ordered list

--- src/main/java/OrderedList.java
+++ src/main/java/OrderedList.java
@@ -29,13 +29,25 @@ public class OrderedList<T>
 
 		public int compare(T v1, T v2)
 		{
-				if (v1 == null) {
-						return (v2 == null) ? 0 : (nullFirst ? -1 : 1);
-				} else if (v2 == null) {
-						return nullFirst ? 1: -1;
-				} else {
-						return (real == null) ? 0 : real.compare(a, b);
-				}
+				if(v1 instanceof Integer)
+						return Integer.compare((Integer)v1, (Integer)v2);
+				else
+						if(v1 instanceof Long)
+								return Long.compare((Long)v1, (Long)v2);
+						else
+								if(v1 instanceof Double)
+										return Double.compare((Double)v1, (Double)v2);
+								else
+										if(v1 instanceof Float)
+												return Float.compare((Float)v1, (Float)v2);
+										else
+												if(v1 instanceof Byte)
+														return Float.compare((Byte)v1, (Byte)v2);
+												else
+														if(v1 instanceof Short)
+																return Float.compare((Short)v1, (Short)v2);
+														else
+																return ((String)v1).trim().compareTo(((String)v2).trim());
 				// -1 если v1 < v2
 				// 0 если v1 == v2
 				// +1 если v1 > v2
@@ -45,45 +57,108 @@ public class OrderedList<T>
 		{
 				// автоматическая вставка value
 				// в нужную позицию
-				Node<T> node = head;
+				Node<T> node = tail;
 				Node<T> newNode = new Node<>(value);
 				if(node == null)
 				{
 						head = newNode;
+						tail = newNode;
 				}
 				else
 				{
-						while(node.next != null)
+						if(compare(value, node.value) < 0)
 						{
-								Node<T> nextNode = node.next;
-								if(compare(value, node.value) > 0 && compare(value, node.next.value) <= 0)
+								node = head;
+								if(compare(value, head.value) < 0)
 								{
-										node.next = newNode;
-										nextNode.prev = newNode;
-										newNode.next = nextNode;
-										newNode.prev = node;
+										node.prev = newNode;
+										head = newNode;
+										head.next = node;
 										return;
 								}
-								node = node.next;
+								else
+										while(node.next != null)
+										{
+												Node<T> nextNode = node.next;
+												if(compare(value, node.value) >= 0 && compare(value, node.next.value) < 1)
+												{
+														node.next = newNode;
+														nextNode.prev = newNode;
+														newNode.next = nextNode;
+														newNode.prev = node;
+														return;
+												}
+												node = node.next;
+										}
 						}
 						node.next = newNode;
+						newNode.prev = node;
+						this.tail = newNode;
 				}
 		}
 
 		public Node<T> find(T val)
 		{
-				return null; // здесь будет ваш код
+				// здесь будет ваш код
+				Node<T> node = head;
+				while(node != null)
+				{
+						if(node.value == val)
+								return node;
+						node = node.next;
+				}
+				return null;
 		}
 
 		public void delete(T val)
 		{
 				// здесь будет ваш код
+				if(this.head == null)
+						return;
+				if(this.head.value == val)
+				{
+						this.head = this.head.next;
+						if(this.head == null)
+								this.tail = null;
+						else
+								this.head.prev = null;
+						return;
+				}
+				if(this.tail.value == val)
+				{
+						this.tail = this.tail.prev;
+						if(this.tail == null)
+						{
+								this.head = null;
+						}
+						else
+								this.tail.next = null;
+						return;
+				}
+				Node node = this.head.next;
+				Node prevNode = this.head;
+				while(node != null)
+				{
+						if(node.value == val)
+						{
+								prevNode.next = node.next;
+								if(prevNode.next == null)
+										this.tail = prevNode;
+								else
+										prevNode.next.prev = prevNode;
+								return;
+						}
+						prevNode = node;
+						node = node.next;
+				}
 		}
 
 		public void clear(boolean asc)
 		{
 				_ascending = asc;
 				// здесь будет ваш код
+				this.head = null;
+				this.tail = null;
 		}
 
 		public int count()
--- test/OrderedListTests.java
+++ test/OrderedListTests.java
@@ -9,9 +9,79 @@ public class OrderedListTests
 		{
 				OrderedList<Integer> orderedList = new OrderedList<>(true);
 				orderedList.add(10);
+				orderedList.add(10);
+				orderedList.add(11);
+				orderedList.add(11);
+				orderedList.add(10);
+				orderedList.add(5);
+				assertEquals(orderedList.head.value.intValue(), 5);
+				assertEquals(orderedList.tail.value.intValue(), 11);
+				assertEquals(orderedList.count(), 6);
+		}
+
+		@Test
+		public void testDeleteElement()
+		{
+				OrderedList<Integer> orderedList = new OrderedList<>(true);
+				orderedList.add(10);
 				orderedList.add(20);
 				orderedList.add(30);
 				orderedList.add(15);
-				assertEquals(orderedList.head.next.value.intValue(), 15);
+				orderedList.delete(20);
+				assertEquals(orderedList.tail.value.intValue(), 30);
 		}
+
+		@Test
+		public void testClearList()
+		{
+				OrderedList<Integer> orderedList = new OrderedList<>(true);
+				orderedList.add(10);
+				orderedList.add(20);
+				orderedList.add(30);
+				orderedList.add(15);
+				orderedList.clear(true);
+				assertNull(orderedList.head);
+				assertNull(orderedList.tail);
+				assertEquals(orderedList.count(), 0);
+		}
+
+		@Test
+		public void testCount()
+		{
+				OrderedList<Integer> orderedList = new OrderedList<>(true);
+				orderedList.add(10);
+				orderedList.add(20);
+				orderedList.add(30);
+				orderedList.add(15);
+				assertEquals(orderedList.count(), 4);
+				orderedList.add(10);
+				orderedList.add(20);
+				orderedList.add(30);
+				orderedList.add(15);
+				assertEquals(orderedList.count(), 8);
+		}
+
+		@Test
+		public void testFind()
+		{
+				OrderedList<Integer> orderedList = new OrderedList<>(true);
+				orderedList.add(10);
+				orderedList.add(20);
+				orderedList.add(30);
+				orderedList.add(15);
+				assertEquals(orderedList.find(30).value.intValue(), 30);
+		}
+
+		@Test
+		public void testGetAll()
+		{
+				OrderedList<Integer> orderedList = new OrderedList<>(true);
+				orderedList.add(10);
+				orderedList.add(20);
+				orderedList.add(30);
+				orderedList.add(15);
+				assertEquals(orderedList.getAll().size(), 4);
+		}
+
+
 }
