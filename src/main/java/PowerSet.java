01a63e906c4849336217f00f68b4ab3aaf821c2f
Nikitin_AA <nikitin_aa@businessculture.ru>
2021-05-08 21:05:20 +0400

    added PowerSet class

---
+++ src/main/java/PowerSet.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PowerSet
+{
+		private List<String> set;
+
+		public List<String> getSet()
+		{
+				return set;
+		}
+
+		public void setSet(List<String> set)
+		{
+				this.set = set;
+		}
+
+		public PowerSet()
+		{
+				// ваша реализация хранилища
+				set = new ArrayList<>();
+		}
+
+		public int size()
+		{
+				// количество элементов в множестве
+				return set.size();
+		}
+
+		public void put(String value)
+		{
+				// всегда срабатывает
+				if(!get(value))
+						set.add(value);
+		}
+
+		public boolean get(String value)
+		{
+				// возвращает true если value имеется в множестве,
+				// иначе false
+				return set.contains(value);
+		}
+
+		public boolean remove(String value)
+		{
+				// возвращает true если value удалено
+				// иначе false
+				if(get(value))
+				{
+						set.remove(value);
+						return get(value);
+				}
+				return false;
+		}
+
+		public PowerSet intersection(PowerSet set2)
+		{
+				// пересечение текущего множества и set2
+				List<String> resultList = new ArrayList<>();
+				for(String element : set2.set)
+				{
+						if(get(element))
+								resultList.add(element);
+				}
+				PowerSet intersectionSet = new PowerSet();
+				intersectionSet.set = resultList;
+				return intersectionSet;
+		}
+
+		public PowerSet union(PowerSet set2)
+		{
+				// объединение текущего множества и set2
+				PowerSet unionSet = new PowerSet();
+				unionSet.set = this.set;
+				unionSet.set.addAll(set2.set);
+				return unionSet;
+		}
+
+		public PowerSet difference(PowerSet set2)
+		{
+				// разница текущего множества и set2
+				List<String> listOfDiff = new ArrayList<>();
+				for(String element : set2.set)
+				{
+						if(!get(element))
+								listOfDiff.add(element);
+				}
+				PowerSet resultSet = new PowerSet();
+				resultSet.set = listOfDiff;
+				return resultSet;
+		}
+
+		public boolean isSubset(PowerSet set2)
+		{
+				// возвращает true, если set2 есть
+				// подмножество текущего множества,
+				// иначе false
+				return set.containsAll(set2.set);
+		}
+}
---
+++ test/PowerSetTests.java
@@ -0,0 +1,101 @@
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class PowerSetTests
+{
+		private String characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		private Random rng = new Random();
+
+		public String getRandomString()
+		{
+				char[] text = new char[10];
+				for(int i = 0; i < 10; i++)
+						text[i] = characters.charAt(rng.nextInt(characters.length()));
+				return new String(text);
+		}
+		@Test
+		public void testPutInSet()
+		{
+				PowerSet powerSet = new PowerSet();
+			  for (int i = 0; i < 20000; i++)
+				{
+						powerSet.put(getRandomString());
+						powerSet.put("First");
+				}
+				assertTrue(powerSet.get("First"));
+				assertEquals(20001, powerSet.getSet().size());
+		}
+
+		@Test
+		public void testRemoveFromSet()
+		{
+				PowerSet powerSet = new PowerSet();
+				powerSet.put("First");
+				powerSet.remove("First");
+				assertEquals(0, powerSet.getSet().size());
+		}
+
+		@Test
+		public void testIntersectionSets()
+		{
+				PowerSet powerSet = new PowerSet();
+				powerSet.put("First");
+				powerSet.put("Second");
+				powerSet.put("Third");
+				PowerSet powerSet1 = new PowerSet();
+				powerSet1.put("Fourth");
+				powerSet1.put("Fifth");
+				powerSet1.put("Second");
+				assertTrue(powerSet.intersection(powerSet1).get("Second"));
+		}
+
+		@Test
+		public void testDifference()
+		{
+				PowerSet powerSet = new PowerSet();
+				powerSet.put("First");
+				powerSet.put("Second");
+				powerSet.put("Third");
+				PowerSet powerSet1 = new PowerSet();
+				powerSet1.put("Fourth");
+				powerSet1.put("Fifth");
+				powerSet1.put("Second");
+				assertTrue(powerSet.difference(powerSet1).get("Fourth"));
+				assertTrue(powerSet.difference(powerSet1).get("Fifth"));
+		}
+
+		@Test
+		public void testUnionSets()
+		{
+				PowerSet powerSet = new PowerSet();
+				powerSet.put("First");
+				PowerSet powerSet1 = new PowerSet();
+				powerSet1.put("Second");
+				assertEquals(2, powerSet.union(powerSet1).getSet().size());
+				powerSet = new PowerSet();
+				powerSet.put("First");
+				powerSet1 = new PowerSet();
+				assertEquals(1, powerSet.union(powerSet1).getSet().size());
+		}
+
+		@Test
+		public void testIsSubset()
+		{
+				PowerSet powerSet = new PowerSet();
+				powerSet.put("First");
+				powerSet.put("Second");
+				powerSet.put("Third");
+				powerSet.put("Fourth");
+				powerSet.put("Fifth");
+				powerSet.put("Sixth");
+				PowerSet powerSet1 = new PowerSet();
+				powerSet1.put("Fourth");
+				powerSet1.put("Fifth");
+				powerSet1.put("Second");
+				assertTrue(powerSet.isSubset(powerSet1));
+		}
+}
