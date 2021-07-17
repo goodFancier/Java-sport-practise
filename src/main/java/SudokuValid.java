c29ffddc16827ba95844477479c46cc623e3a23b
Nikitin_AA <nikitin_aa@businessculture.ru>
2021-07-17 10:15:23 +0400

    Имена, которых следует избегать

---
+++ src/main/java/SudokuValid.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+
+public class SudokuValid
+{
+		public boolean isValidSudoku(char[][] board)
+		{
+				HashSet<Character> rows = new HashSet<>();
+				rows.add('.');
+				return false;
+		}
+}
