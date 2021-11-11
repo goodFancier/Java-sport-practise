3d9f6253b3e364b38acc78ec2b452149748bb8f6
Nikitin_AA <nikitin_aa@businessculture.ru>
2021-11-11 13:01:04 +0400

    fiox

--- src/main/java/Tree.java
+++ src/main/java/Tree.java
@@ -1,136 +1,133 @@
 import java.util.*;
 import java.util.stream.Collectors;
 
-class Tree
+class SimpleTreeNode<T>
 {
-		public class SimpleTreeNode<T>
+		public T NodeValue; // значение в узле
+
+		public T getNodeValue()
 		{
-				public T NodeValue; // значение в узле
-
-				public T getNodeValue()
-				{
-						return NodeValue;
-				}
-
-				public SimpleTreeNode<T> Parent; // родитель или null для корня
-
-				public List<SimpleTreeNode<T>> Children; // список дочерних узлов или null
-
-				public SimpleTreeNode(T val, SimpleTreeNode<T> parent)
-				{
-						NodeValue = val;
-						Parent = parent;
-						Children = null;
-				}
+				return NodeValue;
 		}
 
-		class SimpleTree<T>
+		public SimpleTreeNode<T> Parent; // родитель или null для корня
+
+		public List<SimpleTreeNode<T>> Children; // список дочерних узлов или null
+
+		public SimpleTreeNode(T val, SimpleTreeNode<T> parent)
 		{
-				public SimpleTreeNode<T> Root; // корень, может быть null
+				NodeValue = val;
+				Parent = parent;
+				Children = null;
+		}
+}
 
-				public SimpleTree(SimpleTreeNode<T> root)
-				{
-						Root = root;
-				}
+class SimpleTree<T>
+{
+		public SimpleTreeNode<T> Root; // корень, может быть null
 
-				public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild)
-				{
-						if(NewChild == Root)
-								return;
-						// ваш код добавления нового дочернего узла существующему ParentNode
-						if(ParentNode.Children == null)
-								ParentNode.Children = new ArrayList<>();
-						ParentNode.Children.add(NewChild);
-						NewChild.Parent = ParentNode;
-				}
+		public SimpleTree(SimpleTreeNode<T> root)
+		{
+				Root = root;
+		}
 
-				public void DeleteNode(SimpleTreeNode<T> NodeToDelete)
-				{
-						// ваш код удаления существующего узла NodeToDelete
-						if(NodeToDelete == null)
-								return;
-						NodeToDelete.Children = null;
-						if(NodeToDelete.Parent != null && NodeToDelete.Parent.Children != null)
-								for(int i = 0; i < NodeToDelete.Parent.Children.size(); i++)
-										if(NodeToDelete.equals(NodeToDelete.Parent.Children.get(i)))
-										{
-												NodeToDelete.Parent.Children.remove(i);
-												break;
-										}
-				}
+		public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild)
+		{
+				if(NewChild == Root)
+						return;
+				// ваш код добавления нового дочернего узла существующему ParentNode
+				if(ParentNode.Children == null)
+						ParentNode.Children = new ArrayList<>();
+				ParentNode.Children.add(NewChild);
+				NewChild.Parent = ParentNode;
+		}
 
-				public List<SimpleTreeNode<T>> GetAllNodes()
-				{
-						// ваш код выдачи всех узлов дерева в определённом порядке;
-						return getAllNodesByRecursion(new ArrayList<>(), Root);
-				}
-
-				public List<SimpleTreeNode<T>> getAllNodesByRecursion(List<SimpleTreeNode<T>> allNodes, SimpleTreeNode<T> headNode)
-				{
-						if(headNode != null)
-						{
-								allNodes.add(headNode);
-								if(headNode.Children != null)
-										for(SimpleTreeNode<T> node : headNode.Children)
-										{
-												headNode = node;
-												getAllNodesByRecursion(allNodes, headNode);
-										}
-						}
-						return allNodes;
-				}
-
-				public List<SimpleTreeNode<T>> FindNodesByValue(T val)
-				{
-						// ваш код поиска узлов по значению
-						return GetAllNodes().stream().filter(o -> o.NodeValue != null && o.NodeValue.equals(val)).collect(Collectors.toList());
-				}
-
-				public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent)
-				{
-						// ваш код перемещения узла вместе с его поддеревом --
-						// в качестве дочернего для узла NewParent
-						if(OriginalNode == Root)
-						{
-								return;
-						}
-						if(NewParent.Children == null)
-								NewParent.Children = new ArrayList<>();
-						if(NewParent.Children.contains(OriginalNode))
-								return;
-						if(OriginalNode.Parent != null)
-								if(OriginalNode.Parent.Children != null)
-										OriginalNode.Parent.Children.remove(OriginalNode);
-						OriginalNode.Parent = NewParent;
-						NewParent.Children.add(OriginalNode);
-				}
-
-				public int Count()
-				{
-						// количество всех узлов в дереве
-						return GetAllNodes().size();
-				}
-
-				public int LeafCount()
-				{
-						// количество листьев в дереве
-						return (int)GetAllNodes().stream().filter(o -> o.Children == null || o.Children.isEmpty()).count();
-				}
-
-				public ArrayList<T> EvenTrees()
-				{
-						ArrayList<T> resultList = new ArrayList<>();
-						List<SimpleTreeNode<T>> nodes = GetAllNodes();
-						for(int i = 1; i < nodes.size() - 1; i++)
-						{
-								List<SimpleTreeNode<T>> subTree = getAllNodesByRecursion(new ArrayList<>(), nodes.get(i));
-								if(subTree.size() % 2 == 0)
+		public void DeleteNode(SimpleTreeNode<T> NodeToDelete)
+		{
+				// ваш код удаления существующего узла NodeToDelete
+				if(NodeToDelete == null)
+						return;
+				NodeToDelete.Children = null;
+				if(NodeToDelete.Parent != null && NodeToDelete.Parent.Children != null)
+						for(int i = 0; i < NodeToDelete.Parent.Children.size(); i++)
+								if(NodeToDelete.equals(NodeToDelete.Parent.Children.get(i)))
 								{
-										resultList.add((T)nodes.get(i - 1));
-										resultList.add((T)nodes.get(i));
+										NodeToDelete.Parent.Children.remove(i);
+										break;
 								}
-						}
-						return resultList;
+		}
+
+		public List<SimpleTreeNode<T>> GetAllNodes()
+		{
+				// ваш код выдачи всех узлов дерева в определённом порядке;
+				return getAllNodesByRecursion(new ArrayList<>(), Root);
+		}
+
+		public List<SimpleTreeNode<T>> getAllNodesByRecursion(List<SimpleTreeNode<T>> allNodes, SimpleTreeNode<T> headNode)
+		{
+				if(headNode != null)
+				{
+						allNodes.add(headNode);
+						if(headNode.Children != null)
+								for(SimpleTreeNode<T> node : headNode.Children)
+								{
+										headNode = node;
+										getAllNodesByRecursion(allNodes, headNode);
+								}
 				}
+				return allNodes;
+		}
+
+		public List<SimpleTreeNode<T>> FindNodesByValue(T val)
+		{
+				// ваш код поиска узлов по значению
+				return GetAllNodes().stream().filter(o -> o.NodeValue != null && o.NodeValue.equals(val)).collect(Collectors.toList());
+		}
+
+		public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent)
+		{
+				// ваш код перемещения узла вместе с его поддеревом --
+				// в качестве дочернего для узла NewParent
+				if(OriginalNode == Root)
+				{
+						return;
+				}
+				if(NewParent.Children == null)
+						NewParent.Children = new ArrayList<>();
+				if(NewParent.Children.contains(OriginalNode))
+						return;
+				if(OriginalNode.Parent != null)
+						if(OriginalNode.Parent.Children != null)
+								OriginalNode.Parent.Children.remove(OriginalNode);
+				OriginalNode.Parent = NewParent;
+				NewParent.Children.add(OriginalNode);
+		}
+
+		public int Count()
+		{
+				// количество всех узлов в дереве
+				return GetAllNodes().size();
+		}
+
+		public int LeafCount()
+		{
+				// количество листьев в дереве
+				return (int)GetAllNodes().stream().filter(o -> o.Children == null || o.Children.isEmpty()).count();
+		}
+
+		public ArrayList<T> EvenTrees()
+		{
+				ArrayList<T> resultList = new ArrayList<>();
+				List<SimpleTreeNode<T>> nodes = GetAllNodes();
+				for(int i = 1; i < nodes.size() - 1; i++)
+				{
+						List<SimpleTreeNode<T>> subTree = getAllNodesByRecursion(new ArrayList<>(), nodes.get(i));
+						if(subTree.size() % 2 == 0)
+						{
+								resultList.add((T)nodes.get(i - 1));
+								resultList.add((T)nodes.get(i));
+						}
+				}
+				return resultList;
 		}
 }
