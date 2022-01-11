95cec98714ea9ece453839ada36ac2ae8fec4410
Nikitin_AA <nikitin_aa@businessculture.ru>
2022-01-11 22:32:36 +0400

    sorting

---
+++ src/main/java/SortLevel.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class SortLevel
+{
+		public static void SelectionSortStep(int array[], int i)
+		{
+				if (i >= array.length - 1 || i < 0)
+						return;
+				int min = array[i];
+				int resultIndex = i;
+				if (i < array.length - 1)
+				{
+						for(int j = i + 1; j < array.length; j++)
+						{
+								if(j < array.length - 1)
+								{
+										if(array[j] < array[j + 1])
+										{
+												min = array[j];
+												resultIndex = j;
+										}
+								}
+								else
+										if(array[j] < min)
+										{
+												min = array[j];
+												resultIndex = j;
+										}
+						}
+						if(min < array[i])
+						{
+								int c = array[i];
+								array[i] = min;
+								array[resultIndex] = c;
+						}
+				}
+		}
+
+		public static Boolean BubbleSortStep(int array[])
+		{
+				for(int i = 1; i < array.length - 1; i++)
+						if(array[i] < array[i - 1])
+						{
+								int c = array[i];
+								array[i] = array[i - 1];
+								array[i - 1] = c;
+								return false;
+						}
+				return true;
+		}
+}
---
+++ test/EasySortTests.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class EasySortTests
+{
+		@Test
+		public void testSelectionSortStep()
+		{
+				int[] array = new int[]{10, 40, 30, 25};
+				SortLevel.SelectionSortStep(array, 1);
+				Arrays.stream(array).forEach(System.out::println);
+		}
+
+		@Test
+		public void testBubbleSortStep()
+		{
+				int[] array = new int[]{9, 7, 2};
+				SortLevel.BubbleSortStep(array);
+				Arrays.stream(array).forEach(System.out::println);
+		}
+}
